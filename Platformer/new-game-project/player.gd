3b1b5842cbb1528893344b15e962bb1c175fcede
Louis Alder <louisalder1@gmail.com>
2024-12-27 22:41:41 +0000

    Physics applied to textures

--- Platformer/new-game-project/player.gd
+++ Platformer/new-game-project/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
@@ -12,12 +11,17 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+
 		velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	
 	if direction:
+
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
