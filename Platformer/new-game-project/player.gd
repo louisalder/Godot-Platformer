b4000728e4d7d9d9831f2c36f89e4de0a78489ef
Louis Alder <louisalder1@gmail.com>
2025-01-06 23:11:58 +0000

    Added backgrounds

--- Platformer/new-game-project/player.gd
+++ Platformer/new-game-project/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
+#defining variables for gravity and speed
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+
+#creates a variable for the player
 @onready var player = $AnimatedSprite2D
 
-func _process(delta):
+func _rotate_player(delta):
 	if Input.is_action_pressed("ui_right"):
 		player.flip_h = false  # Face right
 	elif Input.is_action_pressed("ui_left"):
@@ -16,6 +19,10 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 
+# melee_attack doesn't work yet. Need to find out how to detect keypress
+#func _melee_attack(delta):
+#	if Input.is_action_pressed("q"):
+#		player.play("Attack")
 
 	
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -23,21 +30,26 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		
 		if direction != 0:
+		# if movement is not null, the player does the run animation.
 			velocity.x = direction * SPEED
 			player.play("Run")
 			
 		if direction == 0:
 			velocity.x=0
-			player.play("Idle")  # Play the "Idle" animation
+			player.play("Idle")  # if not, it plays the "Idle" animation
 			
-
-			
-		if Input.is_action_just_pressed("ui_accept"):
+		# if the user presses ui_accept, the player jumps
+		elif Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
 			player.play("Jump")
-			
+
+# doesn't work yet		
+#if movement isn't null, and player isn't on the floor
+	if direction != 0 and not is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		velocity.x = direction * SPEED
+		
 
 
-			
 
 	move_and_slide()
