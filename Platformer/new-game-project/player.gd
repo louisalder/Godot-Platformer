ee497179655ebe8f5b8e2945c1d409d5c16d2b0c
Louis Alder <louisalder1@gmail.com>
2025-01-08 21:38:22 +0000

    Updated level map

--- Platformer/new-game-project/player.gd
+++ Platformer/new-game-project/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+<<<<<<< Updated upstream
 #defining variables for gravity and speed
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
@@ -8,17 +9,37 @@ const JUMP_VELOCITY = -300.0
 @onready var player = $AnimatedSprite2D
 
 func _rotate_player(delta):
+=======
+# Define speed and jump velocity
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+const GRAVITY = 500.0
+
+# Variable to access for player sprite
+@onready var player = $AnimatedSprite2D
+
+func _rotate_player():
+	# Flip the sprite based on the movement direction
+>>>>>>> Stashed changes
 	if Input.is_action_pressed("ui_right"):
 		player.flip_h = false  # Face right
 	elif Input.is_action_pressed("ui_left"):
 		player.flip_h = true  # Face left
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Handles movement left and right
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * SPEED
+
+	# Apply gravity when not on the floor
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += GRAVITY * delta
 
+	# Jump logic
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
+<<<<<<< Updated upstream
 # melee_attack doesn't work yet. Need to find out how to detect keypress
 #func _melee_attack(delta):
 #	if Input.is_action_pressed("q"):
@@ -27,9 +48,12 @@ func _physics_process(delta: float) -> void:
 	
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
+=======
+	# Animation if statements
+>>>>>>> Stashed changes
 	if is_on_floor():
-		
 		if direction != 0:
+<<<<<<< Updated upstream
 		# if movement is not null, the player does the run animation.
 			velocity.x = direction * SPEED
 			player.play("Run")
@@ -51,5 +75,15 @@ func _physics_process(delta: float) -> void:
 		
 
 
+=======
+			player.play("Run")
+		else:
+			player.play("Idle")
+	else:
+		player.play("Jump")
 
+	_rotate_player()
+>>>>>>> Stashed changes
+
+	# Move the character and resolve collisions
 	move_and_slide()
