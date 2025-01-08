5f9eef5af47b618e59526f3e9200aef3e8c0c876
Louis Alder <louisalder1@gmail.com>
2025-01-08 21:19:48 +0000

    Updates to character movement
    Resolved collision issues with tree and fixed falling animation.

--- Platformer/new-game-project/player.gd
+++ Platformer/new-game-project/player.gd
@@ -1,55 +1,43 @@
 extends CharacterBody2D
 
-#defining variables for gravity and speed
+# Define speed and jump velocity
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+const GRAVITY = 500.0
 
-#creates a variable for the player
+# Variable to access for player sprite
 @onready var player = $AnimatedSprite2D
 
-func _rotate_player(delta):
+func _rotate_player():
+	# Flip the sprite based on the movement direction
 	if Input.is_action_pressed("ui_right"):
 		player.flip_h = false  # Face right
 	elif Input.is_action_pressed("ui_left"):
 		player.flip_h = true  # Face left
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Handles movement left and right
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * SPEED
+
+	# Apply gravity when not on the floor
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += GRAVITY * delta
 
-
-# melee_attack doesn't work yet. Need to find out how to detect keypress
-#func _melee_attack(delta):
-#	if Input.is_action_pressed("q"):
-#		player.play("Attack")
-
-	
-	var direction := Input.get_axis("ui_left", "ui_right")
-	
-	if is_on_floor():
-		
-		if direction != 0:
-		# if movement is not null, the player does the run animation.
-			velocity.x = direction * SPEED
-			player.play("Run")
-			
-		if direction == 0:
-			velocity.x=0
-			player.play("Idle")  # if not, it plays the "Idle" animation
-			
-		# if the user presses ui_accept, the player jumps
-		elif Input.is_action_just_pressed("ui_accept"):
-			velocity.y = JUMP_VELOCITY
-			player.play("Jump")
-
-# doesn't work yet		
-#if movement isn't null, and player isn't on the floor
-	if direction != 0 and not is_on_floor():
+	# Jump logic
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		velocity.x = direction * SPEED
-		
 
+	# Animation if statements
+	if is_on_floor():
+		if direction != 0:
+			player.play("Run")
+		else:
+			player.play("Idle")
+	else:
+		player.play("Jump")
 
+	_rotate_player()
 
+	# Move the character and resolve collisions
 	move_and_slide()
